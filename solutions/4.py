acd7f852097ed31980b4ba4739236d9be08244a8
jeckett <jeckett@cs.washington.edu>
2022-12-03 22:14:42 -0800

    Day 4 solution

---
+++ solutions/4.py
@@ -0,0 +1,24 @@
+import re
+
+
+def overlap_1(a, b, c, d):
+    return (a <= c <= b and a <= d <= b) or (c <= a <= d and c <= b <= d)
+
+
+def overlap_2(a, b, c, d):
+    return a <= c <= b or a <= d <= b or c <= a <= d or c <= b <= d
+
+
+def solution(overlap):
+    with open("../data/4.txt") as f:
+        prog = re.compile("(\\d+)-(\\d+),(\\d+)-(\\d+)")
+        count = 0
+        for line in f:
+            nums = [int(n) for n in prog.match(line).groups()]
+            if overlap(*sorted(nums[:2]), *sorted(nums[2:])):
+                count += 1
+        return count
+
+
+print(solution(overlap_1))
+print(solution(overlap_2))
