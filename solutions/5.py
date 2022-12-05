92f21205287ced13b5d61848cf8ea13d1760c9bd
jeckett <jeckett@cs.washington.edu>
2022-12-04 22:10:30 -0800

    Day 5 solution

---
+++ solutions/5.py
@@ -0,0 +1,28 @@
+import re
+
+
+def solution(reverse):
+    with open("../data/5.txt") as f:
+        stacks = []
+        for line in f:
+            if line[1] == "1":
+                break
+            for i in range(len(line) // 4):
+                if i == len(stacks):
+                    stacks.append([])
+                if (c := line[i * 4 + 1]) != " ":
+                    stacks[i].insert(0, c)
+        move_re = re.compile(r"move (\d+) from (\d+) to (\d+)")
+        for line in f:
+            if match := move_re.match(line):
+                n, src, dest = map(int, match.groups())
+                crates = stacks[src - 1][-n:]
+                if reverse:
+                    crates = reversed(crates)
+                stacks[dest - 1] += crates
+                del stacks[src - 1][-n:]
+        return ''.join(stack[-1] for stack in stacks)
+
+
+print(solution(reverse=True))
+print(solution(reverse=False))
