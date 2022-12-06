67c7ded128956f17cb4fbe5ef734ed6a770b4ba9
jeckett <jeckett@cs.washington.edu>
2022-12-06 11:54:56 -0800

    Refactor day 6 to be a little bit nicer

--- solutions/6.py
+++ solutions/6.py
@@ -1,9 +1,9 @@
 def solution(num_chars):
     with open("../data/6.txt") as f:
         line = next(iter(f))
-        for i in range(len(line) - num_chars):
-            if len(set(line[i:i + num_chars])) == num_chars:
-                return i + num_chars
+        for i in range(num_chars, len(line)):
+            if len(set(line[i - num_chars:i])) == num_chars:
+                return i
 
 
 print(solution(4))
