18d7e071bf98b47c76b41f1fbf4946d9a592f3d7
jeckett <jeckett@cs.washington.edu>
2022-12-12 15:37:20 -0800

    Code cleanup and performance improvements

--- aoc/2022/12.py
+++ aoc/2022/12.py
@@ -1,3 +1,4 @@
+import collections
 import itertools
 
 import utils
@@ -16,17 +17,20 @@ def moves(starting_chars):
     with utils.get_input(2022, 12) as f:
         height_map = [list(line.strip()) for line in f]
 
-    starting_locations = [(r, c) for r in range(len(height_map)) for c in range(len(height_map[0])) if
-                          height_map[r][c] in starting_chars]
-    nodes = {start: 0 for start in starting_locations}
+    rows, cols = len(height_map), len(height_map[0])
+    starting_locations = [(r, c) for r in range(rows) for c in range(cols) if height_map[r][c] in starting_chars]
+    q = collections.deque(starting_locations)
+    visited = set(q)
     for num_moves in itertools.count(start=1):
-        for r, c in list(nodes.keys()):
+        for _ in range(len(q)):
+            r, c = q.popleft()
             for new_r, new_c in [(r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1)]:
-                if 0 <= new_r < len(height_map) and 0 <= new_c < len(height_map[0]) and height(
-                        height_map[new_r][new_c]) <= height(height_map[r][c]) + 1:
-                    nodes.setdefault((new_r, new_c), num_moves)
+                if new_r in range(rows) and new_c in range(cols) and height(height_map[new_r][new_c]) <= height(
+                        height_map[r][c]) + 1 and (new_r, new_c) not in visited:
                     if height_map[new_r][new_c] == 'E':
                         return num_moves
+                    q.append((new_r, new_c))
+                    visited.add((new_r, new_c))
 
 
 print(moves('S'))  # Part 1
